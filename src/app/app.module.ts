85afe9fcdb4087dc7514ae9299f155b83da291c5
arumugasundar <arumugasundar29@gmail.com>
2022-05-06 13:57:33 +0530

    Application Structure Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BudgetComponent } from './components/budget/budget.component';
+import { PaymentReminderComponent } from './components/payment-reminder/payment-reminder.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FaqsComponent } from './components/faqs/faqs.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    BudgetComponent,
+    PaymentReminderComponent,
+    ProfileComponent,
+    FaqsComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ForgotPasswordComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/payment-reminder/payment-reminder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-reminder',
+  templateUrl: './payment-reminder.component.html',
+  styleUrls: ['./payment-reminder.component.css']
+})
+export class PaymentReminderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
